1e79ebc75d345d3a260764c516a3d19435b7a8f1
Breno Oliveira dos Santos <breno.oliver07@gmail.com>
2018-09-04 15:18:56 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,15 @@
+fipe-cli
+========
+
+Aplicação simples em Node.JS para consultar a tabela fipe em linha de comando
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/fipe-cli.svg)](https://npmjs.org/package/fipe-cli)
+[![Downloads/week](https://img.shields.io/npm/dw/fipe-cli.svg)](https://npmjs.org/package/fipe-cli)
+[![License](https://img.shields.io/npm/l/fipe-cli.svg)](https://github.com/brenoos/fipe-cli/blob/master/package.json)
+
+<!-- toc -->
+# Usage
+<!-- usage -->
+# Commands
+<!-- commands -->
---
+++ src/index.js
@@ -0,0 +1,33 @@
+const {Command, flags} = require('@oclif/command')
+const chalk = require('chalk')
+const clear = require('clear')
+const figlet = require('figlet')
+const emoji = require('node-emoji')
+
+class FipeCliCommand extends Command {
+  async run() {
+    const {flags} = this.parse(FipeCliCommand)
+    clear()
+    this.log(
+      chalk.cyan.bold(
+        figlet.textSync('     FIPE', { horizontalLayout: 'full' })
+      )
+    )
+    this.log(emoji.emojify(':car:'), chalk.yellow.bold("Bem vindo ao sistema de consulta da Tabela FIPE"), emoji.emojify(':racing_motorcycle:'))
+  }
+}
+
+FipeCliCommand.description = `Describe the command here
+...
+Extra documentation goes here
+`
+
+FipeCliCommand.flags = {
+  // add --version flag to show CLI version
+  version: flags.version({char: 'v'}),
+  // add --help flag to show CLI version
+  help: flags.help({char: 'h'}),
+  name: flags.string({char: 'n', description: 'name to print'}),
+}
+
+module.exports = FipeCliCommand
